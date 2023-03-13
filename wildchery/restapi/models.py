6c7a48c97f0d8d46a98d17c40246b8b222806765
Dalaskor <dalas.prog@gmail.com>
2023-03-13 17:18:49 +0400

    Create models

--- wildchery/restapi/models.py
+++ wildchery/restapi/models.py
@@ -22,3 +22,71 @@ def create_user_profile(sender, instance, created, **kwargs):
 @receiver(post_save, sender=User)
 def save_user_profile(sender, instance, **kwargs):
     instance.profile.save()
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=50, unique=True)
+
+    class Meta:
+        ordering = ['name']
+
+    def __str__(self):
+        return self.name
+
+
+class SubCategory(models.Model):
+    name = models.CharField(max_length=50, unique=True)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE, related_name='sub_category')
+    
+
+class Product(models.Model):
+    name = models.CharField(max_length=50)
+    description = models.TextField()
+    price = models.IntegerField()
+    sub_category = models.ForeignKey(SubCategory, on_delete=models.CASCADE, related_name='product')
+
+    class Meta:
+        ordering = ['name']
+
+    def __str__(self):
+        return f"{self.name} - {self.price} Р."
+
+
+class ProductPhoto(models.Model):
+    image = models.ImageField()
+    product = models.ForeignKey(Product, on_delete=models.CASCADE, related_name='product_photo')
+
+
+class Cart(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='cart')
+
+class CartProduct(models.Model):
+    cart = models.ForeignKey(Cart, on_delete=models.CASCADE, related_name='cart_product')
+    product = models.ForeignKey(Product, on_delete=models.CASCADE, related_name='cart_product')
+
+
+class Review(models.Model):
+    description = models.TextField()
+    rating = models.FloatField()
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='review')
+    product = models.ForeignKey(Product, on_delete=models.CASCADE, related_name='review')
+
+
+class Delivery(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='delivery')
+
+
+
+class DeliveryProduct(models.Model):
+    delivery = models.ForeignKey(Delivery, on_delete=models.CASCADE, related_name='delivery_product')
+    product = models.ForeignKey(Product, on_delete=models.CASCADE, related_name='delivery_product')
+
+
+class Favorites(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='favorites')
+
+
+class FavoritesProduct(models.Model):
+    favorites = models.ForeignKey(Favorites, on_delete=models.CASCADE, related_name='favorites_product')
+    product = models.ForeignKey(Product, on_delete=models.CASCADE, related_name='favorites_product')
+
